ef9c552f383bbab630d0b10b651d0bb469cdeaa3
Xinyi Ding <dxywill@gmail.com>
2017-06-21 19:49:21 +0800

    Model changes

--- collection/admin.py
+++ collection/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import EyeImage
+from .models import EyeImage, Survey
 
 # Register your models here.
 admin.site.register(EyeImage)
+admin.site.register(Survey)
--- collection/models.py
+++ collection/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime 
 
 # Create your models here.
 class EyeImage(models.Model):
 	participant = models.ForeignKey(User, on_delete=models.CASCADE)
 	image_id = models.IntegerField()
 	eye_image = models.ImageField(upload_to = 'img/')
+	dateTime = models.DateTimeField(auto_now_add=True)
+	image_name = models.CharField(max_length=200, default='')
+
+class Survey(models.Model):
+	image = models.ForeignKey(EyeImage, on_delete=models.CASCADE)
+	age = models.IntegerField()
+	sex = models.IntegerField()
+	feelings = models.IntegerField()
+	diagnosis = models.IntegerField()
+	medications = models.CharField(max_length=200)
+	substances = models.IntegerField()
+	flash = models.IntegerField()
+
+
