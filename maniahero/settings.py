f8ccc9522bf8c63634a762ce3f9dfb4f61ab9053
Xinyi Ding <dxywill@gmail.com>
2017-07-21 16:08:49 -0500

    add requirements.txt

--- maniahero/settings.py
+++ maniahero/settings.py
@@ -77,16 +77,28 @@ WSGI_APPLICATION = 'maniahero.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'maniahero',
-        'USER': 'mania',
-        'PASSWORD': 'theManiaPassword$',
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'maniahero',
+            'USER': 'mania',
+            'PASSWORD': 'theManiaPassword$',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
