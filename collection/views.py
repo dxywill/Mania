cbeeab172af8ad24f9d6f28567bb198fcb8b3b25
Xinyi Ding <dxywill@gmail.com>
2017-08-05 15:51:43 -0500

    fix issue #13

--- collection/views.py
+++ collection/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 import time
-from PIL import Image
+from PIL import Image, ExifTags
 import os, random
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
@@ -17,68 +17,68 @@ import boto3
 # Create your views here.
 
 def landing(request):
-	#return render(request, 'collection/landing_new.html')
-	return redirect('/accounts/login/')
+    #return render(request, 'collection/landing_new.html')
+    return redirect('/accounts/login/')
 
 def index(request):
-	if request.method == 'POST':
-		form = ImageUploadForm(request.POST, request.FILES)
-		if form.is_valid():
-			eye_image = EyeImage(eye_image=request.FILES['image'])
-			eye_image.participant = request.user
-			eye_image.image_id = 2
-			eye_image.save()
-			context_data = {'image': eye_image }
-			return render(request, 'collection/upload.html', context_data)
-			
-	return render(request, 'collection/home.html')
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            eye_image = EyeImage(eye_image=request.FILES['image'])
+            eye_image.participant = request.user
+            eye_image.image_id = 2
+            eye_image.save()
+            context_data = {'image': eye_image }
+            return render(request, 'collection/upload.html', context_data)
+            
+    return render(request, 'collection/home.html')
 
 @login_required
 def upload(request):
-	return render(request, 'collection/upload.html')
+    return render(request, 'collection/upload.html')
 
 @login_required
 def complete(request):
-	#Get survey data
-	if request.method == 'POST':
-		mentalOptions = request.POST.getlist('mentalOptions')
-		mental = 0
-		for m in mentalOptions:
-			mental += int(m)
-		medicationOptions = request.POST.getlist('medicationOptions')
-		medication = 0
-		for m in medicationOptions:
-			medication += int(m)
-		medicationOther = request.POST.get('medicationOther')
-		substancesOptions = request.POST.getlist('substancesOptions')
-		substances = 0
-		for s in substancesOptions:
-			substances += int(s)
-		if request.POST.get('otherOptions'):
-			flash = int(request.POST.get('otherOptions'))
-		else:
-			flash = -1
-		survey = Survey()
-		imageId =  request.session.get('eye_image')
-		survey.image = EyeImage.objects.get(id=imageId)
-		survey.feelings = mental
-		if request.POST.get('stateOptions'):
-			survey.state = int(request.POST.get('stateOptions'))
-		else:
-			survey.state = -1
-		survey.medications = str(medication) + medicationOther
-		survey.substances = substances
-		survey.flash = flash
-		survey.save() 
-	logout(request)
-	return render(request, 'collection/complete.html')
+    #Get survey data
+    if request.method == 'POST':
+        mentalOptions = request.POST.getlist('mentalOptions')
+        mental = 0
+        for m in mentalOptions:
+            mental += int(m)
+        medicationOptions = request.POST.getlist('medicationOptions')
+        medication = 0
+        for m in medicationOptions:
+            medication += int(m)
+        medicationOther = request.POST.get('medicationOther')
+        substancesOptions = request.POST.getlist('substancesOptions')
+        substances = 0
+        for s in substancesOptions:
+            substances += int(s)
+        if request.POST.get('otherOptions'):
+            flash = int(request.POST.get('otherOptions'))
+        else:
+            flash = -1
+        survey = Survey()
+        imageId =  request.session.get('eye_image')
+        survey.image = EyeImage.objects.get(id=imageId)
+        survey.feelings = mental
+        if request.POST.get('stateOptions'):
+            survey.state = int(request.POST.get('stateOptions'))
+        else:
+            survey.state = -1
+        survey.medications = str(medication) + medicationOther
+        survey.substances = substances
+        survey.flash = flash
+        survey.save() 
+    logout(request)
+    return render(request, 'collection/complete.html')
 
 
 def faq(request):
-	return render(request, 'collection/faq.html')
+    return render(request, 'collection/faq.html')
 
 def consent(request):
-	return render(request, 'collection/consent.html')
+    return render(request, 'collection/consent.html')
 
 
 def encrypt(key, filename):
@@ -104,80 +104,99 @@ def encrypt(key, filename):
 
 @login_required
 def survey(request):
-	# x = float(request.POST['x_offset'])
-	# y = float(request.POST['y_offset'])
-	# width = float(request.POST['width'])
-	# height = float(request.POST['height'])
+    x = float(request.POST['x_offset'])
+    y = float(request.POST['y_offset'])
+    width = float(request.POST['width'])
+    height = float(request.POST['height'])
 
-	# right_low_x = x + width
-	# right_low_y = y + height
+    right_low_x = x + width
+    right_low_y = y + height
 
 
-	# ts = str(time.time())
+    ts = str(time.time())
 
-	# request.FILES['file0'].name = request.user.username + '_' + ts + '.png'
-	# eye_image = EyeImage(eye_image=request.FILES['file0'])
-	# eye_image.participant = request.user
-	# eye_image.image_id = 2
-	# eye_image.image_name = request.user.username + '_' + ts + '.png'
-	# eye_image.save()
-	# request.session['eye_image'] = eye_image.id
+    request.FILES['file0'].name = request.user.username + '_' + ts + '.png'
+    eye_image = EyeImage(eye_image=request.FILES['file0'])
+    eye_image.participant = request.user
+    eye_image.image_id = 2
+    eye_image.image_name = request.user.username + '_' + ts + '.png'
+    eye_image.save()
+    request.session['eye_image'] = eye_image.id
 
-	# #Crop image
-	# im = Image.open(request.FILES['file0'])
-	# if im.size[0] > im.size[1]:
-	# 	im = im.transpose(Image.ROTATE_270)
-	# box = (x, y, right_low_x, right_low_y)
+    #Crop image
+    im = Image.open(request.FILES['file0'])
+    
+    #Need to rotate image if necessary
+    try:
+        for orientation in ExifTags.TAGS.keys(): 
+            if ExifTags.TAGS[orientation]=='Orientation':
+                break
+        exif=dict(im._getexif().items())
 
-	# cropped = im.crop(box)
-	# crop_path = settings.MEDIA_ROOT + '/crop/' + request.user.username + '_' + ts + '_crop.png'
-	# cropped.save(crop_path, 'png')
+        if  exif[orientation] == 3 : 
+            im=im.rotate(180, expand=True)
+        elif exif[orientation] == 6 : 
+            im=im.rotate(270, expand=True)
+        elif exif[orientation] == 8 : 
+            im=im.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        # cases: image don't have getexif
+        #traceback.print_exc()
+        pass
 
-	# rt_path = 'crop/' + request.user.username + '_' + ts + '_crop.png'
-	# eye_image_crop = EyeImage(eye_image=rt_path)
-	# eye_image_crop.participant = request.user
-	# eye_image_crop.image_id = 3
-	# eye_image_crop.image_name = request.user.username + '_' + ts + '_crop.png'
-	# eye_image_crop.save()
+    # if im.size[0] > im.size[1]:
+    #   im = im.transpose(Image.ROTATE_270)
+    box = (x, y, right_low_x, right_low_y)
 
-	# #encrypt the image
-	# password = "theImagePassword$"
-	# hasher = SHA256.new(password.encode("latin-1"))
-	# digest = hasher.digest()
-	# filename = crop_path
-	# encrypt(digest, filename)
+    cropped = im.crop(box)
+    crop_path = settings.MEDIA_ROOT + '/crop/' + request.user.username + '_' + ts + '_crop.png'
+    cropped.save(crop_path, 'png')
 
-	# #upload to S3
-	# if 'AWS_ACCESS_KEY_ID' in os.environ:	
-	# 	s3 = boto3.client(
-	#     	's3',
-	#     	aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-	#     	aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
-	# 	)
-	# else:
-	# 	s3 = boto3.client(
-	#     	's3'
-	# 	)
+    rt_path = 'crop/' + request.user.username + '_' + ts + '_crop.png'
+    eye_image_crop = EyeImage(eye_image=rt_path)
+    eye_image_crop.participant = request.user
+    eye_image_crop.image_id = 3
+    eye_image_crop.image_name = request.user.username + '_' + ts + '_crop.png'
+    eye_image_crop.save()
 
-	# filename = crop_path
-	# bucket_name = 'smu-mania-study-images'
-	# s3.upload_file(filename, bucket_name, 'images/{}'.format(request.user.username + '_' + ts + '_crop.png'))
+    #encrypt the image
+    password = "theImagePassword$"
+    hasher = SHA256.new(password.encode("latin-1"))
+    digest = hasher.digest()
+    filename = crop_path
+    encrypt(digest, filename)
 
-	return render(request, 'collection/survey.html')
+    #upload to S3
+    if 'AWS_ACCESS_KEY_ID' in os.environ:   
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+            aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+        )
+    else:
+        s3 = boto3.client(
+            's3'
+        )
+
+    filename = crop_path
+    bucket_name = 'smu-mania-study-images'
+    s3.upload_file(filename, bucket_name, 'images/{}'.format(request.user.username + '_' + ts + '_crop.png'))
+
+    return render(request, 'collection/survey.html')
 
 @login_required
 def profile(request):
-	#Get profile data
-	if request.method == 'POST':
-		age = int(request.POST.get('agerange'))
-		gender = int(request.POST.get('gender'))
-		diagnosis = int(request.POST.get('diagnosis'))
-		profile = Profile()
-		profile.user = request.user
-		profile.age = age
-		profile.gender = gender
-		profile.diagnosis = diagnosis
-		profile.save()
-		return redirect('/collection/upload')
+    #Get profile data
+    if request.method == 'POST':
+        age = int(request.POST.get('agerange'))
+        gender = int(request.POST.get('gender'))
+        diagnosis = int(request.POST.get('diagnosis'))
+        profile = Profile()
+        profile.user = request.user
+        profile.age = age
+        profile.gender = gender
+        profile.diagnosis = diagnosis
+        profile.save()
+        return redirect('/collection/upload')
 
-	return render(request, 'collection/profile.html')
+    return render(request, 'collection/profile.html')
